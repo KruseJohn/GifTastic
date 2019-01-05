5c01ba7419806c5d8611bc81a7b64412a05733ba
JKruse <kruse67@live.com>
2019-01-05 15:59:05 -0700

    1/5/19

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // Array for topics...
     var topics = ["Spider-Man", "Iron Man", "Batman", "Wolverine", "Black Panther", "The Hulk", "Superman", "Wonder Woman", "Thor", "Captain America",
-     "Black Widow", "Aquaman", "Deadpool", "Catwoman", "Hellboy"];
+        "Black Widow", "Aquaman", "Deadpool", "Catwoman", "Hellboy"];
 
     //  create buttons for the above array
     function renderButtons() {
@@ -16,15 +16,15 @@ $(document).ready(function () {
             a.text(topics[i]);
             $("#gif-buttons").append(a);
 
-        }  // end of for loop...
+        } // end of for loop...
 
-    }  // end of renderButtons function...
+    } // end of renderButtons function...
 
     //  call the function  
     renderButtons();
 
-    // This function handles events when the add a superhero submit button is clicked
-    $("#add-gif").on("click", function(event) {
+    // This function handles events when the "add a superhero submit button" is clicked
+    $("#add-gif").on("click", function (event) {
         event.preventDefault();
         // This line grabs the input from the textbox
         var topic = $("#gif-input").val().trim();
@@ -32,16 +32,45 @@ $(document).ready(function () {
         // Adding the new gif (topic) from the textbox to our topic array
         topics.push(topic);
         console.log(topics);
+
+        // This prevents an empty text box from being submitted
+        if ($("#gif-input").val() === "")  {
+            return false;
+        }
         // Calling renderButtons which handles the processing of our topic array
         renderButtons();
-      });
+    });
+
+    // This function is for a secret button on the webpage...
+    $("#friends").click(function () {
+        
+        // Alert user upon finding the secret button...
+        swal({
+            title: 'Congratulations!!!',
+            text: 'You have found the secret button.  A new "Marvel Studio" button has been created and the gifs will automatically appear!!!',
+            imageUrl: 'https://media.giphy.com/media/l4FGni1RBAR2OWsGk/giphy.gif',
+            imageWidth: 500,
+            imageHeight: 250,
+            //  animation: false,
+            customClass: 'animated flash',
+        });
+
+        var secret = "Marvel Studios";
+        topics.push(secret);
+        renderButtons();
+        $(".superhero").trigger("click");
+        
+    }); // end of secret button function
 
     // Event listener for all button elements
     $(document).on("click", ".superhero", function () {
 
         // Hide our batman greeter and his friends when a superhero button is selected
         $("#batman").hide();
-        $("#friends").hide();
+
+        //  Move the #friends gif below the new gifs on the page 
+        $("#friends").appendTo("#friends2");
+
         // In this case, the "this" keyword refers to the button that was clicked and
         // variable "hero" will log data from each clicked button...
         var hero = $(this).html();
@@ -72,6 +101,7 @@ $(document).ready(function () {
                     // Creating a div for the gif
                     var gifDiv = $("<div class='d-inline'>");
                     gifDiv.addClass("holder");
+    
                     // Add variable rating and store it
                     var rating = results[i].rating;
 
@@ -85,11 +115,11 @@ $(document).ready(function () {
                     //  Creating a new variable to include data from heroImage and stillImage
                     var gifHero = $("<img>").attr("src", stillImage).attr("data-animate", heroImage).attr("data-still", stillImage).css("width", "100%").css("height", "auto");
                     gifHero.attr("data-state", "still");
-        
+
                     //  Display the gif in the html
                     gifDiv.append(p);
                     gifDiv.prepend(gifHero);
-                
+
                     $("#gifs-appear-here").append(gifDiv);
 
                     //  on.click function when gifHero is pressed
@@ -116,15 +146,11 @@ $(document).ready(function () {
 
         } // end of on-click function...
 
-        
+
 
     }); // end of document on-click...
 
-    $("#friends").click(function(){
-        alert("image is clicked"); 
-        renderButtons();
-       
-    });
+
 
 
 }); // end of document.ready function...
