c020c59fdb42a24f1f09e737ff0d7bfdb7b9a1bf
JKruse <kruse67@live.com>
2019-01-08 13:19:04 -0700

    1/8/19

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     // Array for topics...
     var topics = ["Spider-Man", "Iron Man", "Batman", "Wolverine", "Black Panther", "The Hulk", "Superman", "Wonder Woman", "Thor", "Captain America",
-        "Black Widow", "Aquaman", "Deadpool", "Catwoman", "Hellboy"];
+        "Black Widow", "Aquaman", "Deadpool", "Catwoman", "Hellboy"
+    ];
 
     //  create buttons for the above array
     function renderButtons() {
@@ -34,33 +35,63 @@ $(document).ready(function () {
         console.log(topics);
 
         // This prevents an empty text box from being submitted
-        if ($("#gif-input").val() === "")  {
+        if ($("#gif-input").val() === "") {
             return false;
         }
         // Calling renderButtons which handles the processing of our topic array
         renderButtons();
     });
 
-    // This function is for a secret button on the webpage...
+    // This function is for the favorite button on the webpage...
     $("#friends").click(function () {
-        
-        // Alert user upon finding the secret button...
+
+        // Alert for navigation to "favorites" page...
         swal({
-            title: 'Congratulations!!!',
-            text: 'You have found the secret button.  A new "Marvel Studio" button has been created and the gifs will automatically appear!!!',
-            imageUrl: 'https://media.giphy.com/media/l4FGni1RBAR2OWsGk/giphy.gif',
-            imageWidth: 500,
-            imageHeight: 250,
+            title: 'Favorites Page',
+            text: 'Click the favorites button on any Superhero Gif to remove it from this page.',
+            imageUrl: 'assets/images/justice.jpg',
+            imageWidth: 400,
+            imageHeight: 200,
             //  animation: false,
             customClass: 'animated flash',
         });
 
-        var secret = "Marvel Studios";
-        topics.push(secret);
-        renderButtons();
-        $(".superhero").trigger("click");
-        
-    }); // end of secret button function
+        // Show and hide on "favorites" page
+        $("#return").show();
+        $("#dead").show();
+        $("#spinner").show();
+        $("#favGifs").show();
+        $("#gifs-appear-here").hide();
+        $("#gif-buttons").hide();
+        $("#search").hide();
+        $("#friends").hide();
+        $(".jumbotron").hide();
+
+        // This empty function prevents the "favorites" window from popping up after the first click
+            window.swal = function() {};
+
+        //  When user clicks "return to main page" button, show and hide on main page...
+        $("#return").on("click", function () {
+
+            $("#return").hide();
+            $("#dead").hide();
+            $("#spinner").hide();
+            $("#favGifs").hide();
+            $("#gifs-appear-here").show();
+            $("#gif-buttons").show();
+            $("#search").show();
+            $("#friends").show();
+            $(".jumbotron").show();
+        });
+
+    }); // end of favorite button function
+
+    // Hide the permanent items found on "favorites" page
+    //  $("#add").hide();
+    //  $("#counter").hide();
+    $("#return").hide();
+    $("#dead").hide();
+    $("#spinner").hide();
 
     // Event listener for all button elements
     $(document).on("click", ".superhero", function () {
@@ -74,10 +105,11 @@ $(document).ready(function () {
         // In this case, the "this" keyword refers to the button that was clicked and
         // variable "hero" will log data from each clicked button...
         var hero = $(this).html();
+
         console.log(hero);
 
-        // Constructing a URL to search Giphy for the name of the person who said the quote
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=VQVE4TWxa31vYL0rVQmC5oiSRQgC4taw&limit=10&rating=pg";
+        // Constructing a URL to search Giphy for each hero after selected by user
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=VQVE4TWxa31vYL0rVQmC5oiSRQgC4taw&limit=5&rating=pg";
         console.log(queryURL);
         // Performing our AJAX GET request
         $.ajax({
@@ -86,7 +118,7 @@ $(document).ready(function () {
             })
 
             // After the data comes back from the API
-            .done(function (response) {
+            .then(function (response) {
 
                 // Storing an array of results in the results variable
                 var results = response.data;
@@ -98,15 +130,21 @@ $(document).ready(function () {
                 // Looping over every result item
                 for (var i = 0; i < results.length; i++) {
 
-                    // Creating a div for the gif
-                    var gifDiv = $("<div class='d-inline'>");
-                    gifDiv.addClass("holder");
-    
+                    var idTag = hero + i;
+                    idTagFixed = idTag.split(' ').join('');
+
+                    // Creating a div for the gif                  
+                    var gifDiv = $("<div>", {
+                        class: 'holder',
+                        id: idTagFixed
+                    });
+                    //  gifDiv.addClass("holder");
+
                     // Add variable rating and store it
                     var rating = results[i].rating;
 
                     // Create a line tag with the result item's rating, and display into html
-                    var p = $('<p>').text("Rating: " + rating);
+                    var p = $('<p>').text("Rating: " + rating).css("color", "wheat");
 
                     // Creating variables for the gif
                     var heroImage = results[i].images.fixed_height.url;
@@ -116,10 +154,17 @@ $(document).ready(function () {
                     var gifHero = $("<img>").attr("src", stillImage).attr("data-animate", heroImage).attr("data-still", stillImage).css("width", "100%").css("height", "auto");
                     gifHero.attr("data-state", "still");
 
-                    //  Display the gif in the html
-                    gifDiv.append(p);
-                    gifDiv.prepend(gifHero);
+                    // create a div and button for a favorite section
 
+                    var favBtn = $("<i class='far fa-heart fa-lg'></i>");
+                    favBtn.attr({
+                        'favorite-status': 'No',
+                        'data-parent': idTagFixed
+                    }).css("float", "right").css("color", "red");
+
+                    //  Display the gif in the html
+                    gifDiv.append(p, favBtn);
+                    gifDiv.prepend(gifHero);
                     $("#gifs-appear-here").append(gifDiv);
 
                     //  on.click function when gifHero is pressed
@@ -127,7 +172,7 @@ $(document).ready(function () {
 
                 } // end of for loop...
 
-            }); // end of done response...
+            }); // end of done response..
 
         // Create function to stop and animate gifs
         function animateGif() {
@@ -138,7 +183,7 @@ $(document).ready(function () {
             if (state === "still") {
                 $(this).attr("src", $(this).data("animate"));
                 $(this).attr("data-state", "animate");
-
+                $(this).css("hover", "pointer");
             } else {
                 $(this).attr("src", $(this).data("still"));
                 $(this).attr("data-state", "still");
@@ -146,11 +191,41 @@ $(document).ready(function () {
 
         } // end of on-click function...
 
-
-
     }); // end of document on-click...
 
+    // On click function for selection of gif as "favorite"...
+    $(document.body).on("click", ".fa-heart", function () {
+        var favStatus = $(this).attr("favorite-status");
+        var parentCard = $(this).attr("data-parent");
+        var parentCardID = "#" + parentCard;
+        console.log(parentCardID);
 
+        // Add to Favorites section
+        if (favStatus === "No") {
+            $(this).addClass("fas").removeClass("far");
+            $(this).attr({
+                'favorite-status': 'Yes'
+            });
+            var newFavCard = $("<div>", {
+                id: "fav" + parentCard,
+                class: "holder"
+            });
+            $(newFavCard).append($(parentCardID).html());
+            $("#favGifs").append(newFavCard);
+            $("#favGifs").hide();
+            console.log(newFavCard);
+
+
+        } else {
+            // Remove from Favorites
+            $("[data-parent=" + parentCard + "]").attr({
+                'favorite-status': 'No'
+            }).addClass("far").removeClass("fas");
+            var removeFav = $("#fav" + parentCard);
+            $(removeFav).remove();
+        }
+
+    });  // end of document.body function
 
 
 }); // end of document.ready function...
